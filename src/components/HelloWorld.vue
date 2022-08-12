f5a40d6ce981791128ff006861bbbb921db2397c
fr4nkastro <85426216+fr4nkastro@users.noreply.github.com>
2022-08-11 23:27:45 -0600

    Pendiente convertir diagonal en unos

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,124 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest" target="_blank" rel="noopener">unit-jest</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+<div>
+  <div class="divpadre">
+
+
+    <div class="divhijo">
+        <table>
+            <tr><td colspan="2">Matriz A</td></tr>
+            <tr><td><input type="number" v-model="A11"></td><td><input type="number" v-model="A12"></td></tr>
+            <tr><td><input type="number" v-model="A21"></td><td><input type="number" v-model="A22"></td></tr>
+        </table>
+
+    </div >
+     <div class="divhijo">
+        <table>
+            <tr><td colspan="2">Matriz B</td></tr>
+            <tr><td><input type="number" v-model="B11"></td><td><input type="number" v-model="B12"></td></tr>
+            <tr><td><input type="number" v-model="B21"></td><td><input type="number" v-model="B22"></td></tr>
+        </table>
+
+    </div>
+ 
+
   </div>
+<div>
+    <input type="button" value="Calcular" @click="calcular">
+
+</div>
+</div>
+
 </template>
 
 <script>
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  data(){
+        return{
+        A11:1,
+        A12:3,
+        A21:2,
+        A22:4,
+
+        B11:1,
+        B12:0,
+        B21:0,
+        B22:1,
+
+        MatrizA:null,
+        MatrizB:null
+      
+     }
+   },
+    methods:{
+      rowOperation(matriz, indexRows, values ){
+        var tempMatriz= matriz.slice();
+        for (let indexColumn = 0; indexColumn < matriz.length; indexColumn++) {
+          matriz[indexRows[0]][indexColumn]= tempMatriz[indexRows[0]][indexColumn]*values[0]
+          matriz[indexRows[1]][indexColumn]= tempMatriz[indexRows[1]][indexColumn]*values[1]
+        }
+        for (let indexColumn = 0; indexColumn < matriz.length; indexColumn++) {
+          matriz[indexRows[0]][indexColumn]+= tempMatriz[indexRows[1]][indexColumn]
+        }
+      },
+
+      showArray(matriz){
+        var strOutput=""
+        for (let index = 0; index < this.MatrizA.length; index++) {
+          for (let indexCol = 0; indexCol < this.MatrizA.length; indexCol++) {
+            strOutput+=String(matriz[index][indexCol]) + "\t";
+          }
+          strOutput+="\n"
+          
+        }
+        console.log(strOutput);
+        return strOutput;
+      },
+      calcular(){
+          this.MatrizA=[[this.A11,this.A12],[this.A21,this.A22]];
+          this.MatrizB=[[this.B11,this.B12],[this.B21,this.B22]];
+
+          var tmpMatriz=this.MatrizA.slice();
+          console.log(tmpMatriz);
+          for (let indexRow = 0; indexRow < this.MatrizA.length; indexRow++) {
+            for (let indexColumn = 0; indexColumn < this.MatrizA.length; indexColumn++) {
+              if(indexRow!= indexColumn){
+                var signo= 1;
+                if(indexRow!=this.MatrizA.length-1){
+                  
+                  if(Math.sign(this.MatrizA[indexRow][indexColumn]) ==1 && Math.sign(this.MatrizA[indexRow+1][indexColumn])==1){
+                    signo=-1
+                  }else if(Math.sign(this.MatrizA[indexRow][indexColumn]) ==-1 && Math.sign(this.MatrizA[indexRow+1][indexColumn])==-1){
+                    signo=-1
+                  }
+                  this.rowOperation(this.MatrizA,[indexRow, indexRow+1], [this.MatrizA[indexRow+1][indexColumn]*signo,this.MatrizA[indexRow][indexColumn]])
+                }else if(indexRow== this.MatrizA.length-1){
+                  if(Math.sign(this.MatrizA[indexRow][indexColumn]) ==1 && Math.sign(this.MatrizA[0][indexColumn])==1){
+                    signo=-1
+                  }else if(Math.sign(this.MatrizA[indexRow][indexColumn]) ==-1 && Math.sign(this.MatrizA[0][indexColumn])==-1){
+                    signo=-1
+                  }
+                  this.rowOperation(this.MatrizA,[indexRow, 0], [this.MatrizA[0][indexColumn]*signo, this.MatrizA[indexRow][indexColumn]])
+                }
+              }
+            }              
+          }
+
+        this.showArray(this.MatrizA);
+       }
+
+    }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+.divpadre{
+   display: flex;
+   justify-content: center;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.divhijo{
+    margin: 10px;
 }
 </style>
